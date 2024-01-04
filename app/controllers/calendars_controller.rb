5c8f031cc963a30084d4f49e6a6ce4dc92907c0d
kan9512 <Kanjanawadee.Shiraishi@gmail.com>
2024-01-04 21:32:05 +0900

    Issue4 - Issue6 解消（2回目）

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    getWeek
+    get_Week
     @plan = Plan.all
   end
 
@@ -12,38 +12,40 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    @plan = Plan.new(plan_params)
-
-     if @plan.save
-      redirect_to calendars_path
-     else  
-      render :new
-     end
+    Plan.create(plan_params)
+    redirect_to action: :index
   end
 
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
-  def getWeek
+  def get_Week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
+    
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
-
-    @week_days = []
+        # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
+    @week_days = []
 
     7.times do |x|
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => Date.today.wday}
+
+      wday_num = Date.today.wday + x
+      if wday_num >= 7 #「wday_numが7以上の場合」という条件式
+         wday_num = wday_num -7
+      end
+
+      #days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday:wdays[Date.today.wday + x ]}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday:wdays[wday_num]}
       @week_days.push(days)
     end
 
